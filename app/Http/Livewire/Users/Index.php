0d52df54855fe6ca8aad001f338b77f73ebc6c1f
Marlom <marlon_alvezz@hotmail.com>
2022-06-24 17:46:04 -0300

    add users livewire

---
+++ app/Http/Livewire/Users/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\Users;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+
+    public function render()
+    {
+        return view('livewire.users.index', [
+            'users' => User::query()->paginate(10),
+        ]);
+    }
+}
